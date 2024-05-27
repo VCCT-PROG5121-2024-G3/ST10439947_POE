02355e67887e13a065ca957272086120c12af673
Atlas <ST10439947@vcconnect.edu.za>
2024-05-27 23:01:02 +0200

    New Main Class

---
+++ src/ProjectRoom/MainClass.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ProjectRoom;
+
+/**
+ *
+ * @author Amelia ST10439947@vcconnect.edu.za ST10439947
+ */
+public class MainClass {
+
+    /**
+     * @param args the command line arguments
+     */
+    public void main(String[] args) {
+
+    }
+
+}
