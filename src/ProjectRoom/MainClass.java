4b32a7a10fda338d1bd0e1ba79d5b1184a39f343
Atlas <ST10439947@vcconnect.edu.za>
2024-05-28 21:47:43 +0200

    New, Updated and Working MainClass

--- src/ProjectRoom/MainClass.java
+++ src/ProjectRoom/MainClass.java
@@ -1,8 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package ProjectRoom;
+
 
 /**
  *
@@ -14,6 +11,9 @@ public class MainClass {
      * @param args the command line arguments
      */
     public void main(String[] args) {
+        WelcomeClass welcome = new WelcomeClass();
+        //Housekeeping()
+       welcome.WelcomeMenu();
 
     }
 
