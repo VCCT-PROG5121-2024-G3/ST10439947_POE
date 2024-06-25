2e7a9bfe8f2fa07b002998e4b97c8b8a553bd0f3
Atlas <ST10439947@vcconnect.edu.za>
2024-06-25 10:23:58 +0200

    Added pictures and backgrounds, changed some fonts and sizes as well

--- src/ProjectRoom/WelcomeClass.java
+++ src/ProjectRoom/WelcomeClass.java
@@ -1,10 +1,10 @@
 package ProjectRoom;
 
 //imports used for the  JOptionPane window and the custom image icon
-import javax.swing.ImageIcon;
-import javax.swing.JOptionPane;
-//-------------------------------------------------------------------------------------------------------------------------------------------------
+import java.awt.*;
+import javax.swing.*;
 
+//-------------------------------------------------------------------------------------------------------------------------------------------------
 /**
  *
  * @author Amelia ST10439947@vcconnect.edu.za ST10439947
@@ -18,6 +18,7 @@ public class WelcomeClass {
 
     public void WelcomeMenu() {
         RegisterClass register = new RegisterClass();
+        Image backgroundImage = new ImageIcon("Pics/background.jpg").getImage();
 
         //I use custom images for all the JOptionPane windows, this is how i fetch the images
         ImageIcon stardrop = new ImageIcon("Pics/stardrop.png");
@@ -25,13 +26,54 @@ public class WelcomeClass {
         //custom register button
         Object[] options = {"Register"};
 
-        int option = JOptionPane.showOptionDialog(null, "Welcome to EasyKanban!", "Welcome!",
-                JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE,
-                stardrop, options, options[0]);
-        //checks to see if user pressed the button, then takes them to the registration page
-        if (option == 0) {
+        BackgroundPanel backgroundPanel = new BackgroundPanel(backgroundImage);
+        backgroundPanel.setLayout(null);
+
+        JLabel welcomeLabel = new JLabel("Welcome to EasyKanban!");
+        welcomeLabel.setBounds(25, 25, 280, 30);
+        welcomeLabel.setFont(new Font("Arial", Font.BOLD, 16));
+        backgroundPanel.add(welcomeLabel, BorderLayout.CENTER);
+
+        JButton registerButton = new JButton("Register!");
+        registerButton.setBounds(70, 70, 100, 30);
+        registerButton.addActionListener(e -> {
+            JOptionPane.getRootFrame().dispose();
             register.RegisterScreen();
+        });
+        backgroundPanel.add(registerButton, BorderLayout.CENTER);
+
+        JDialog welcomeDialog = new JDialog();
+        welcomeDialog.setTitle("Welcome!");
+        welcomeDialog.setModal(true);
+        welcomeDialog.setSize(250, 180);
+        welcomeDialog.setLocationRelativeTo(null);
+        welcomeDialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+        welcomeDialog.setIconImage(stardrop.getImage());
+
+        welcomeDialog.add(backgroundPanel);
+        welcomeDialog.setVisible(true);
+
+        //checks to see if user pressed the button, then takes them to the registration page
+    }
+
+    //---------------------------------------------------------------------------------------------------------------------------------------------
+    //---------------------------------------------------------------------------------------------------------------------------------------------
+    public class BackgroundPanel extends JPanel {
+
+        private Image backgroundImage;
+
+        public BackgroundPanel(Image backgroundImage) {
+            this.backgroundImage = backgroundImage;
+            setOpaque(false);
+        }
+
+        @Override
+        protected void paintComponent(Graphics g) {
+            super.paintComponent(g);
+            g.drawImage(backgroundImage, 0, 0, getWidth(), getHeight(), this);
         }
     }
+    //---------------------------------------------------------------------------------------------------------------------------------------------
+    //---------------------------------------------------------------------------------------------------------------------------------------------
 }
 //------------------------------------------------------------END OF FILE------------------------------------------------------------------
